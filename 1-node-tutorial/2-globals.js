cbe98dce1f7f99f02c338c4397898929fbceea68
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-03-16 12:57:34 +0000

    Module

---
+++ 1-node-tutorial/2-globals.js
@@ -0,0 +1,26 @@
+//en
+//Globals (variables) - No window !!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (Common JS)
+// module     - info about current module (file)
+// process    - info env where the program is being executed
+
+
+
+
+//fr
+//Globals (variables) - Pas de fenêtre !!!
+
+// __dirname    - chemin vers le répertoire courant
+// __filename   - nom du fichier
+// require      - fonction pour utiliser les modules (Common JS)
+// module       - informations sur le module actuel (fichier)
+// process      - informations sur l'environement( le systeme d'exploitation ) où le programme est en cours d'exécution
+
+console.log(__dirname);
+
+setInterval(() => {
+    console.log("Hello world")
+}, 1000);
--- 1-node-tutorial/app.js
+++ 1-node-tutorial/app.js
@@ -1,20 +1,10 @@
-//en
-//Globals (variables) - No window !!!
+// Module
 
-// __dirname  - path to current directory
-// __filename - file name
-// require    - function to use modules (Common JS)
-// module     - info about current module (file)
-// process    - info env where the program is being executed
+const sayHi = (name) => {
+    console.log(`Hi ${name}`);
+}
 
+sayHi("Cherif Diouf");
+sayHi(`Abdoul`);
+sayHi('Maleeq');
 
-
-
-//fr
-//Globals (variables) - Pas de fenêtre !!!
-
-// __dirname    - chemin vers le répertoire courant
-// __filename   - nom du fichier
-// require      - fonction pour utiliser les modules (Common JS)
-// module       - informations sur le module actuel (fichier)
-// process      - info env où le programme est en cours d'exécution
