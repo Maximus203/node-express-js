b9c3a28d8662d21b0654a3c25bfb727d81963643
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-03-16 12:47:07 +0000

    Complements

---
+++ 1-node-tutorial/app.js
@@ -0,0 +1,20 @@
+//en
+//Globals (variables) - No window !!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (Common JS)
+// module     - info about current module (file)
+// process    - info env where the program is being executed
+
+
+
+
+//fr
+//Globals (variables) - Pas de fenêtre !!!
+
+// __dirname    - chemin vers le répertoire courant
+// __filename   - nom du fichier
+// require      - fonction pour utiliser les modules (Common JS)
+// module       - informations sur le module actuel (fichier)
+// process      - info env où le programme est en cours d'exécution
